f80f210d608620c2853b4cdde8c58f2f8b542ea9
tomoya <tomoya.kamaji@gmail.com>
2023-06-27 08:57:18 +0900

    update cors

--- app/config/gin.go
+++ app/config/gin.go
@@ -11,7 +11,6 @@ func NewGinEngine() *gin.Engine {
 	router := gin.Default()
 	config := cors.DefaultConfig()
 	config.AllowOrigins = []string{"*"}
-	config.AllowHeaders = []string{"Authorization", "Origin", "Content-Length", "Content-Type"}
 
 	router.Use(cors.New(config))
 	return router
