ab3e01c648c4185125171e24b9983e335a592c6b
hiroaki-u <rokumugi123good@gmail.com>
2023-08-03 22:22:14 +0900

    refactor/route.goにルーターを共通化

--- app/server/route/route.go
+++ app/server/route/route.go
@@ -3,7 +3,10 @@ package route
 import (
 	ginpkg "github.com/gin-gonic/gin"
 
+	userApp "github/code-kakitai/code-kakitai/application/user"
+	"github/code-kakitai/code-kakitai/infrastructure/mysql/repository"
 	health_handler "github/code-kakitai/code-kakitai/presentation/health_handler"
+	userPre "github/code-kakitai/code-kakitai/presentation/user"
 )
 
 func InitRoute(api *ginpkg.Engine) {
@@ -13,4 +16,14 @@ func InitRoute(api *ginpkg.Engine) {
 	{
 		userRoute(v1)
 	}
+}
+
+func userRoute(r *ginpkg.RouterGroup) {
+	userRepository := repository.NewUserRepository()
+	h := userPre.NewHandler(
+		userApp.NewFindUserUseCase(userRepository),
+		userApp.NewSaveUserUseCase(userRepository),
+	)
+	group := r.Group("/users")
+	group.GET("/:id", h.GetUserByID)
 }
