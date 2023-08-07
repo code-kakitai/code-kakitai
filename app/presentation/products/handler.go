55cb4c3b25613d72871eb6526830b29c59282fad
reiji11011 <reiji.sugama@gmail.com>
2023-08-07 20:43:00 +0900

    add/商品登録ハンドラー追加

--- app/presentation/products/handler.go
+++ app/presentation/products/handler.go
@@ -1,3 +1,29 @@
+package products
+
+import (
+	"github.com/gin-gonic/gin"
+	"github/code-kakitai/code-kakitai/application/product"
+	"github/code-kakitai/code-kakitai/presentation/settings"
+)
+
+type handler struct {
+	saveProductUseCase *product.SaveProductUseCase
+}
+
+func NewHandler(saveProductUseCase *product.SaveProductUseCase) handler {
+	return handler{
+		saveProductUseCase: saveProductUseCase,
+	}
+}
+
+type PostProductsParams struct {
+	OwnerID     string `json:"owner_id"`
+	Name        string `json:"name"`
+	Description string `json:"description"`
+	Price       int64  `json:"price"`
+	Stock       int    `json:"stock"`
+}
+
 // PostProducts godoc
 // @Summary 商品を保存する
 // @Tags products
@@ -6,3 +32,32 @@
 // @Param request body PostProductsParams true "登録商品"
 // @Success 201 {object} postProductResponse
 // @Router /v1/products [post]
+func (h handler) PostProducts(ctx *gin.Context) {
+	var params PostProductsParams
+	// TODO リクエストのバリデーション
+	err := ctx.ShouldBindJSON(&params)
+	if err != nil {
+		settings.ReturnBadRequest(ctx, err)
+	}
+	p, err := h.saveProductUseCase.Run(
+		ctx,
+		params.OwnerID,
+		params.Name,
+		params.Description,
+		params.Price,
+		params.Stock)
+	if err != nil {
+		settings.ReturnStatusInternalServerError(ctx, err)
+	}
+	response := postProductResponse{
+		productResponseModel{
+			Id:          p.ID(),
+			OwnerID:     p.OwnerID(),
+			Name:        p.Name(),
+			Description: p.Description(),
+			Price:       p.Price(),
+			Stock:       p.Stock(),
+		},
+	}
+	settings.ReturnStatusCreated(ctx, response)
+}
