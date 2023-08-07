ec0ed69749f2146bd81e87eb79f0df948bf21985
reiji11011 <reiji.sugama@gmail.com>
2023-08-07 20:40:03 +0900

    add/Swagger Documentation

---
+++ app/presentation/products/handler.go
@@ -0,0 +1,8 @@
+// PostProducts godoc
+// @Summary 商品を保存する
+// @Tags products
+// @Accept json
+// @Produce json
+// @Param request body PostProductsParams true "登録商品"
+// @Success 201 {object} postProductResponse
+// @Router /v1/products [post]
