2a77ad3f641426c89e4a2a1319cdbf0c879dcc50
tomoya <tomoya.kamaji@gmail.com>
2023-08-13 10:53:03 +0900

    create:usecase

---
+++ app/application/product/fetch_product_use_case.go
@@ -0,0 +1,47 @@
+package product
+
+import (
+	"context"
+)
+
+type FetchProductUseCase struct {
+	FetchProductQueryService FetchProductQueryService
+}
+
+func NewFetchProductUseCase(
+	fetchProductQueryService FetchProductQueryService,
+) *FetchProductUseCase {
+	return &FetchProductUseCase{
+		FetchProductQueryService: fetchProductQueryService,
+	}
+}
+
+type FetchProductUseCaseDto struct {
+	ID          string
+	Name        string
+	Description string
+	Price       int64
+	Stock       int
+	OwnerID     string
+	OwnerName   string
+	OwnerEmail  string
+}
+
+func (uc *FetchProductUseCase) Run(ctx context.Context) ([]*FetchProductUseCaseDto, error) {
+	qsDtos, err := uc.FetchProductQueryService.Run(ctx)
+	var ucDtos []*FetchProductUseCaseDto
+
+	for _, qsDto := range qsDtos {
+		ucDtos = append(ucDtos, &FetchProductUseCaseDto{
+			ID:          qsDto.ID,
+			Name:        qsDto.Name,
+			Description: qsDto.Description,
+			Price:       qsDto.Price,
+			Stock:       qsDto.Stock,
+			OwnerID:     qsDto.OwnerID,
+			OwnerName:   qsDto.OwnerName,
+			OwnerEmail:  qsDto.OwnerEmail,
+		})
+	}
+	return ucDtos, err
+}
