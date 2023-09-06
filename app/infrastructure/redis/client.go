1f307dd00c736949c6cd6a1c29da12c96bff391a
hiroaki-u <rokumugi123good@gmail.com>
2023-09-06 22:34:11 +0900

    refactor/testの接続はtestファイルに書いた

--- app/infrastructure/redis/client.go
+++ app/infrastructure/redis/client.go
@@ -3,7 +3,6 @@ package redis
 import (
 	"time"
 
-	"github.com/alicebob/miniredis"
 	"github.com/redis/go-redis/v9"
 
 	"github/code-kakitai/code-kakitai/config"
@@ -20,18 +19,6 @@ func GetRedisClient() *redis.Client {
 func NewClient(conf config.Redis) *redis.Client {
 	redisClient = redis.NewClient(&redis.Options{
 		Addr:                  conf.Host + ":" + conf.Port,
-		ReadTimeout:           3 * time.Second,
-		WriteTimeout:          3 * time.Second,
-		ContextTimeoutEnabled: true,
-	})
-
-	return redisClient
-}
-
-func NewTestClient() *redis.Client {
-	s, _ := miniredis.Run()
-	redisClient := redis.NewClient(&redis.Options{
-		Addr:                  s.Addr(),
 		ReadTimeout:           3 * time.Second,
 		WriteTimeout:          3 * time.Second,
 		ContextTimeoutEnabled: true,
--- app/infrastructure/redis/repository/common_test.go
+++ app/infrastructure/redis/repository/common_test.go
@@ -2,10 +2,10 @@ package repository
 
 import (
 	"testing"
+	"time"
 
+	"github.com/alicebob/miniredis"
 	redis "github.com/redis/go-redis/v9"
-
-	infraRedis "github/code-kakitai/code-kakitai/infrastructure/redis"
 )
 
 var (
@@ -14,9 +14,21 @@ var (
 
 func TestMain(m *testing.M) {
 	// テスト用Redisのセットアップ
-	redisCli = infraRedis.NewTestClient()
+	redisCli = NewTestClient()
 	defer redisCli.Close()
 
 	// テスト実行
 	m.Run()
+}
+
+func NewTestClient() *redis.Client {
+	s, _ := miniredis.Run()
+	redisClient := redis.NewClient(&redis.Options{
+		Addr:                  s.Addr(),
+		ReadTimeout:           3 * time.Second,
+		WriteTimeout:          3 * time.Second,
+		ContextTimeoutEnabled: true,
+	})
+
+	return redisClient
 }
