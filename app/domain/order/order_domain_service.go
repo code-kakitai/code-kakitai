433f1d939fc0b20eadcadda3d108c706265e8a7c
hiroaki-u <rokumugi123good@gmail.com>
2023-09-16 13:18:16 +0900

    refactor/エラーハンドリングをpkgからdomainに移行

--- app/domain/order/order_domain_service.go
+++ app/domain/order/order_domain_service.go
@@ -4,8 +4,7 @@ import (
 	"context"
 	"time"
 
-	"github.com/code-kakitai/go-pkg/errors"
-
+	domainErr "github/code-kakitai/code-kakitai/domain/error"
 	cartDomain "github/code-kakitai/code-kakitai/domain/cart"
 	productDomain "github/code-kakitai/code-kakitai/domain/product"
 )
@@ -47,7 +46,7 @@ func (ds *orderDomainService) OrderProducts(ctx context.Context, cart *cartDomai
 		ops = append(ops, *op)
 		if !ok {
 			// 購入した商品の商品詳細が見つからない場合はエラー（商品を購入すると同時に、商品が削除された場合等に発生）
-			return "", errors.NewError("商品が見つかりません。")
+			return "", domainErr.NewError("商品が見つかりません。")
 		}
 		if err := p.Consume(cp.Quantity()); err != nil {
 			return "", err
--- app/domain/user/user.go
+++ app/domain/user/user.go
@@ -4,9 +4,10 @@ import (
 	"net/mail"
 	"unicode/utf8"
 
-	"github.com/code-kakitai/go-pkg/errors"
 	"github.com/code-kakitai/go-pkg/strings"
 	"github.com/code-kakitai/go-pkg/ulid"
+
+	domainErr "github/code-kakitai/code-kakitai/domain/error"
 )
 
 type User struct {
@@ -74,22 +75,22 @@ func newUser(
 ) (*User, error) {
 	// 名前のバリデーション
 	if utf8.RuneCountInString(lastName) < nameLengthMin || utf8.RuneCountInString(lastName) > nameLengthMax {
-		return nil, errors.NewError("名前（姓）の値が不正です。")
+		return nil, domainErr.NewError("名前（姓）の値が不正です。")
 	}
 	if utf8.RuneCountInString(firstName) < nameLengthMin || utf8.RuneCountInString(firstName) > nameLengthMax {
-		return nil, errors.NewError("名前（名）の値が不正です。")
+		return nil, domainErr.NewError("名前（名）の値が不正です。")
 	}
 
 	// メールアドレスのバリデーション
 	if _, err := mail.ParseAddress(email); err != nil {
-		return nil, errors.NewError("メールアドレスの値が不正です。")
+		return nil, domainErr.NewError("メールアドレスの値が不正です。")
 	}
 
 	// phoneNumberからハイフンを除く
 	phoneNumber = strings.RemoveHyphen(phoneNumber)
 	// 電話番号のバリデーション
 	if _, ok := phoneNumberDigitMap[utf8.RuneCountInString(phoneNumber)]; !ok {
-		return nil, errors.NewError("電話番号の値が不正です。")
+		return nil, domainErr.NewError("電話番号の値が不正です。")
 	}
 
 	ad, err := newAddress(prefecture, city, addressExtra)
@@ -167,7 +168,7 @@ func newAddress(
 ) (address, error) {
 	// 1つでも空のパラメーターがあればエラー
 	if prefecture == "" || city == "" || extra == "" {
-		return address{}, errors.NewError("住所の値が不正です。")
+		return address{}, domainErr.NewError("住所の値が不正です。")
 	}
 	return address{
 		prefecture: prefecture,
