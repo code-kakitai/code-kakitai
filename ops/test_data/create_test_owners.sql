00c7f76fdf3851a48bcbeb79b54381e2095e15b0
hiroaki-u <rokumugi123good@gmail.com>
2023-10-29 15:52:19 +0900

    update/初期環境構築時にデータベースの作成と初期データinsertを実行するようにした

---
+++ ops/test_data/create_test_owners.sql
@@ -0,0 +1,14 @@
+INSERT INTO owners (
+    id,
+    email,
+    name,
+    created_at,
+    updated_at
+)
+VALUES (
+    '01HCNYK7QVEH4X0RMEXRXEKJSY',
+    'owner-example@owner.com',
+    '古戸垣敦',
+    '2023-08-06 15:01:00',
+    '2023-08-06 15:01:00'
+);
---
+++ ops/test_data/create_test_users.sql
@@ -0,0 +1,24 @@
+INSERT INTO users (
+    id,
+    email,
+    firebaseUid,
+    phone_number,
+    first_name,
+    last_name,
+    postal_code,
+    prefecture,
+    city,
+    address_extra
+)
+VALUES (
+    '01HCNYK0PKYZWB0ZT1KR0EPWGP',
+    'example@test.com',
+    'test',
+    '08011112222',
+    "田中",
+    '太郎',
+    '150-0012',
+    '東京都',
+    '渋谷区',
+    '広尾2-2'
+);
