df807375326fb65bf0ed1e84d86635656e963af0
sugamaan <reiji.sugama@gmail.com>
2023-11-04 17:54:29 +0900

    add/orderRepository Saveのテストを追加

--- app/infrastructure/mysql/repository/order_repository_test.go
+++ app/infrastructure/mysql/repository/order_repository_test.go
@@ -1,9 +1,46 @@
 package repository
 
 import (
+	"context"
+	"log"
 	"testing"
+	"time"
+
+	orderDomain "github/code-kakitai/code-kakitai/domain/order"
 )
 
-func Test_orderRepository_Save_And_Find(t *testing.T) {
-	// todo impl
+func TestOrderRepository_Save(t *testing.T) {
+	orderProduct, err := orderDomain.NewOrderProduct(
+		"01HCNYK4MQNC6G6X3F3DGXZ2J8",
+		1000,
+		1,
+	)
+	now := time.Date(2023, 1, 1, 0, 0, 0, 0, time.Local)
+	order, err := orderDomain.NewOrder(
+		"01HCNYK0PKYZWB0ZT1KR0EPWGP",
+		1000,
+		[]orderDomain.OrderProduct{*orderProduct},
+		now)
+	if err != nil {
+		log.Print(err)
+	}
+	orderRepository := NewOrderRepository()
+
+	tests := []struct {
+		name  string
+		input *orderDomain.Order
+	}{
+		{
+			name:  "保存ができること",
+			input: order,
+		},
+	}
+	resetTestData(t)
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if err := orderRepository.Save(context.Background(), tt.input); err != nil {
+				t.Errorf("Save() error = %v", err)
+			}
+		})
+	}
 }
