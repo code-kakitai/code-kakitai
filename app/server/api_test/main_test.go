71d773f646f05a9f01664f375f1831eb22b994ee
gyu <nousidayo@gmail.com>
2024-03-03 13:36:16 +0900

    create api_test/main_test.go

--- app/server/api_test/main_test.go
+++ app/server/api_test/main_test.go
@@ -1,13 +1,14 @@
-package query_service
+package api_test
 
 import (
-	"testing"
-
-	"gopkg.in/testfixtures.v2"
-
 	"github/code-kakitai/code-kakitai/infrastructure/mysql/db"
 	dbTest "github/code-kakitai/code-kakitai/infrastructure/mysql/db/db_test"
 	"github/code-kakitai/code-kakitai/infrastructure/mysql/db/dbgen"
+	"github/code-kakitai/code-kakitai/presentation/settings"
+	"github/code-kakitai/code-kakitai/server/route"
+	"testing"
+
+	"gopkg.in/testfixtures.v2"
 )
 
 var (
@@ -17,20 +18,18 @@ var (
 func TestMain(m *testing.M) {
 	var err error
 
-	// DBの立ち上げ
 	resource, pool := dbTest.CreateContainer()
 	defer dbTest.CloseContainer(resource, pool)
 
-	// DBへ接続する
 	dbCon := dbTest.ConnectDB(resource, pool)
 	defer dbCon.Close()
 
-	// テスト用DBをセットアップ
-	dbTest.SetupTestDB("../db/schema/schema.sql")
-
-	// テストデータの準備
-	fixturePath := "../fixtures"
-	fixtures, err = testfixtures.NewFolder(dbCon, &testfixtures.MySQL{}, fixturePath)
+	dbTest.SetupTestDB("../../infrastructure/mysql/db/schema/schema.sql")
+	fixtures, err = testfixtures.NewFolder(
+		dbCon,
+		&testfixtures.MySQL{},
+		"../../infrastructure/mysql/fixtures",
+	)
 	if err != nil {
 		panic(err)
 	}
@@ -39,13 +38,8 @@ func TestMain(m *testing.M) {
 	db.SetReadQuery(q)
 	db.SetDB(dbCon)
 
-	// テスト実行
-	m.Run()
-}
+	api := settings.NewGinEngine()
+	route.InitRoute(api)
 
-func resetTestData(t *testing.T) {
-	t.Helper()
-	if err := fixtures.Load(); err != nil {
-		t.Fatal(err)
-	}
+	m.Run()
 }
