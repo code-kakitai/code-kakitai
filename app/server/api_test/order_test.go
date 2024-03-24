da336980f5ec2f4e14ca19983b6063a000086f7d
gyu <nousidayo@gmail.com>
2024-03-24 18:41:17 +0900

    fix TestOrder_PostOrders
    カートがRedisで管理されているため、カート追加処理を事前に実行する

--- app/server/api_test/order_test.go
+++ app/server/api_test/order_test.go
@@ -3,31 +3,35 @@ package api_test
 import (
 	"bytes"
 	"encoding/json"
+	"github/code-kakitai/code-kakitai/presentation/cart"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 
-	"github.com/google/go-cmp/cmp"
+	"github.com/oklog/ulid/v2"
 )
 
 func TestOrder_PostOrders(t *testing.T) {
 	tests := map[string]struct {
-		requestBody  []map[string]any
-		expectedCode int
-		expectedBody []map[string]any
+		postCartsParams cart.PostCartsParams
+		requestBody     []map[string]any
+		expectedCode    int
+		expectedBody    map[string]any
 	}{
 		"正常系": {
+			postCartsParams: cart.PostCartsParams{
+				ProductID: "01HCNYK4MQNC6G6X3F3DGXZ2J8",
+				Quantity:  1,
+			},
 			requestBody: []map[string]any{
 				{
 					"product_id": "01HCNYK4MQNC6G6X3F3DGXZ2J8",
 					"quantity":   1,
 				},
 			},
-			expectedCode: http.StatusOK,
-			expectedBody: []map[string]any{
-				{
-					"id": "01HCNYK4MQNC6G6X3F3DGXZ2J8",
-				},
+			expectedCode: http.StatusCreated,
+			expectedBody: map[string]any{
+				"id": "01HSQWHYEVKRH80JJPBRQ5DCGW",
 			},
 		},
 	}
@@ -36,14 +40,24 @@ func TestOrder_PostOrders(t *testing.T) {
 		t.Run(testName, func(t *testing.T) {
 			resetTestData(t)
 
-			b, err := json.Marshal(tt.requestBody)
+			// Redisに値を入れるために、カートに商品を追加
+			b, err := json.Marshal(tt.postCartsParams)
 			if err != nil {
 				t.Errorf("failed to marshal err: %v", err)
 				t.Fail()
 			}
 			req := httptest.NewRequest(http.MethodPost, "/v1/carts", bytes.NewBuffer(b))
-			req = httptest.NewRequest(http.MethodPost, "/v1/orders", bytes.NewBuffer(b))
 			w := httptest.NewRecorder()
+			api.ServeHTTP(w, req)
+
+			// 注文を作成
+			b, err = json.Marshal(tt.requestBody)
+			if err != nil {
+				t.Errorf("failed to marshal err: %v", err)
+				t.Fail()
+			}
+			req = httptest.NewRequest(http.MethodPost, "/v1/orders", bytes.NewBuffer(b))
+			w = httptest.NewRecorder()
 			api.ServeHTTP(w, req)
 
 			if w.Code != tt.expectedCode {
@@ -52,10 +66,11 @@ func TestOrder_PostOrders(t *testing.T) {
 
 			var actualBody map[string]interface{}
 			if err := json.Unmarshal(w.Body.Bytes(), &actualBody); err != nil {
-				t.Errorf("failed to unmarshal response body: %v", err)
+				t.Fatalf("failed to unmarshal response body: %v", err)
 			}
-			if diff := cmp.Diff(tt.expectedBody, actualBody); diff != "" {
-				t.Errorf("response body mismatch (-want +got):\n%s", diff)
+			// UUIDはランダムな文字列なため、形式のみチェック
+			if _, err := ulid.ParseStrict(actualBody["id"].(string)); err != nil {
+				t.Errorf("id is not a valid ULID: %v", actualBody["id"])
 			}
 		})
 	}
